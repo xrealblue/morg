dffc972bea596cb9a56386ea3f8557efa7197cbb
xRealBlue <heetvavadiya456@gmail.com>
2026-06-11 20:18:41 +0530

    @committ

--- src/app/[owner]/[repo]/commit/[sha]/page.tsx
+++ src/app/[owner]/[repo]/commit/[sha]/page.tsx
@@ -84,7 +84,7 @@ export default function CommitPage({ params }: Props) {
       <div className="flex w-80 shrink-0 flex-col border-r border-zinc-800 bg-zinc-950">
         <div className="border-b border-zinc-800 px-4 py-3">
           <div className="flex items-center gap-2.5">
-            {d.commitAuthorAvatar && (
+            {(d.commitAuthorAvatar as string) && (
               <img
                 src={d.commitAuthorAvatar as string}
                 alt={d.commitAuthorName as string}
